e6ba9fe56b4dc69b2abca34e6ba25bc5a6a0ec85
Emil Oldenburg <emil.oldenburg@tum.de>
2019-05-05 23:36:22 +0200

    Created gotcha demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'bsh-gotcha';
+	constructor(private snackBar: MatSnackBar) {}
+
+	selectedCount(): number {
+		return this.dataset.images.filter(e => e.selected == true).length;
+	}
+
+	correctCount(): number {
+		return this.dataset.images.filter(e => e.selected == e.isValid).length;
+	}
+
+	validate() {
+		this.snackBar.open(
+			"You correctly selected " +
+				this.correctCount() +
+				"/" +
+				this.dataset.images.length +
+				" images."
+		);
+	}
+
+	dataset = {
+		class: "potatoes",
+		images: [
+			{
+				id: 1,
+				src: "/assets/potatoes/potato1.jpg",
+				isValid: true,
+				selected: false
+			},
+			{
+				id: 2,
+				src: "/assets/potatoes/banana.jpg",
+				isValid: false,
+				selected: false
+			},
+			{
+				id: 3,
+				src: "/assets/potatoes/potato2.png",
+				isValid: true,
+				selected: false
+			},
+			{
+				id: 4,
+				src: "/assets/potatoes/carrot.png",
+				isValid: false,
+				selected: false
+			},
+			{
+				id: 5,
+				src: "/assets/potatoes/potato3.jpg",
+				isValid: true,
+				selected: false
+			},
+			{
+				id: 6,
+				src: "/assets/potatoes/potato4.jpg",
+				isValid: true,
+				selected: false
+			},
+			{
+				id: 7,
+				src: "/assets/potatoes/cucumber.jpg",
+				isValid: false,
+				selected: false
+			},
+			{
+				id: 8,
+				src: "/assets/potatoes/potato5.jpg",
+				isValid: true,
+				selected: false
+			},
+			{
+				id: 9,
+				src: "/assets/potatoes/radish.jpg",
+				isValid: false,
+				selected: false
+			}
+		]
+	};
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		MatCardModule,
+		BrowserAnimationsModule,
+		MatGridListModule,
+		MatButtonModule,
+		MatSnackBarModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
