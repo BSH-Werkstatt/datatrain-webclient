71187e1ad65d47b03cd93b8a17bbe1afeb95773a
Emil Oldenburg <emil.oldenburg@tum.de>
2019-05-15 17:19:50 -0700

    route animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, RouterOutlet } from "@angular/router";
+
+import { slideInAnimation } from "./animations/router-animations";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.scss"]
+	styleUrls: ["./app.component.scss"],
+	animations: [
+		slideInAnimation
+		// animation triggers go here
+	]
 })
 export class AppComponent {
 	constructor(private router: Router) {}
+
+	prepareRoute(outlet: RouterOutlet) {
+		return (
+			outlet &&
+			outlet.activatedRouteData &&
+			outlet.activatedRouteData["animation"]
+		);
+	}
 }
